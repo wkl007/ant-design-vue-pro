ecf97fd55d2af0f502e54b561b9752b33698e779
wkl007 <499657357@qq.com>
2021-04-13 20:17:27 +0800

    feat: 监控页面开发

--- src/views/dashboard/monitor/index.vue
+++ src/views/dashboard/monitor/index.vue
@@ -34,12 +34,7 @@
             </a-col>
           </a-row>
           <div class="map-chart">
-            <a-tooltip :title="t('dashboard.monitor.waiting-for-implementation')">
-              <img
-                src="https://gw.alipayobjects.com/zos/rmsportal/HBWnDEUXCnGnGrRfrpKa.png"
-                alt="map"
-              />
-            </a-tooltip>
+            <v-map/>
           </div>
         </a-card>
       </a-col>
@@ -48,13 +43,23 @@
           :title="t('dashboard.monitor.activity-forecast')"
           style="margin-bottom: 24px;"
           :bordered="false"
-        ></a-card>
+        >
+          <a-statistic title="目标评估" value="有望达到预期"/>
+          <v-tiny-area :data="activeData"/>
+          <div class="active-chart-legend">
+            <span>00:00</span>
+            <span>12:00</span>
+            <span>23:00</span>
+          </div>
+        </a-card>
         <a-card
           :title="t('dashboard.monitor.efficiency')"
           style="margin-bottom: 24px;"
           :body-style="{ textAlign: 'center' }"
           :bordered="false"
-        ></a-card>
+        >
+          <v-gauge/>
+        </a-card>
       </a-col>
     </a-row>
     <a-row :gutter="24">
@@ -64,10 +69,16 @@
           :bordered="false"
           class="pie-card"
         >
-          <a-row style="padding: 16px 0;">
-            <a-col :span="8"></a-col>
-            <a-col :span="8"></a-col>
-            <a-col :span="8"></a-col>
+          <a-row style="padding: 50px 0;">
+            <a-col :span="8">
+              <v-ring-progress :percent="0.28" :color="['#60C0FF','#E8EDF3']"/>
+            </a-col>
+            <a-col :span="8">
+              <v-ring-progress :percent="0.2" :color="['#66F4E3','#E8EDF3']"/>
+            </a-col>
+            <a-col :span="8">
+              <v-ring-progress :percent="0.32" :color="['#2FC25B','#E8EDF3']"/>
+            </a-col>
           </a-row>
         </a-card>
       </a-col>
@@ -77,46 +88,87 @@
           :loading="context.loading"
           :bordered="false"
           :body-style="{ overflow: 'hidden' }"
-        ></a-card>
+        >
+          <v-word-cloud :data="context.dataSource"/>
+        </a-card>
       </a-col>
       <a-col :xl="6" :lg="12" :sm="24" :xs="24" style="margin-bottom: 24px;">
         <a-card
           :title="t('dashboard.monitor.resource-surplus')"
           :body-style="{ textAlign: 'center', fontSize: 0 }"
           :bordered="false"
-        ></a-card>
+        >
+          <v-liquid/>
+        </a-card>
       </a-col>
     </a-row>
-    <a-card>
-      <v-tiny-area :data="activeData"/>
-      <v-gauge/>
-      <v-ring-progress/>
-      <v-word-cloud/>
-      <v-liquid/>
-    </a-card>
   </grid-content>
 </template>
 
 <script lang="ts">
-import { defineComponent, reactive } from 'vue'
-import { GridContent, injectProProvider, VTinyArea, VGauge, VRingProgress, VWordCloud, VLiquid } from '@/components'
+import { defineComponent, nextTick, onMounted, onUnmounted, ref } from 'vue'
+import {
+  GridContent,
+  injectProProvider,
+  VGauge,
+  VLiquid,
+  VMap,
+  VRingProgress,
+  VTinyArea,
+  VWordCloud
+} from '@/components'
 import { useFetchData } from '@/hooks'
 import DashboardServer from '@/api/dashboard'
 import { numberFormat } from '@/utils'
 
 function fixedZero (val: number) {
-  return val * 1 < 10 ? `0${val}` : val
+  return val < 10 ? `0${val}` : val
 }
 
 function getActiveData () {
   const activeData = []
   for (let i = 0; i < 24; i += 1) {
-    activeData.push({
-      x: `${fixedZero(i)}:00`,
-      y: Math.floor(Math.random() * 200) + i * 50
-    })
+    activeData.push(Math.floor(Math.random() * 200) + i * 50)
   }
   return activeData
+}
+
+function useLoopData () {
+  const timer = ref<number | undefined>(undefined)
+  const requestRef = ref<number | undefined>(undefined)
+  const activeData = ref<any>(getActiveData())
+
+  const loopData = () => {
+    requestRef.value = requestAnimationFrame(() => {
+      timer.value = setTimeout(() => {
+        activeData.value = getActiveData()
+        nextTick(() => {
+          loopData()
+        })
+      }, 1000)
+    })
+  }
+
+  const clear = () => {
+    clearTimeout(timer.value)
+    if (requestRef.value) {
+      cancelAnimationFrame(requestRef.value)
+    }
+  }
+
+  onMounted(() => {
+    loopData()
+
+    // cancel
+    onUnmounted(() => {
+      clear()
+    })
+  })
+
+  return {
+    clear,
+    activeData
+  }
 }
 
 export default defineComponent({
@@ -127,7 +179,8 @@ export default defineComponent({
     VGauge,
     VRingProgress,
     VWordCloud,
-    VLiquid
+    VLiquid,
+    VMap
   },
   setup () {
     const { i18n: t } = injectProProvider()
@@ -141,10 +194,12 @@ export default defineComponent({
       { loading: false }
     )
 
+    const { activeData } = useLoopData()
+
     return {
       t,
       context,
-      activeData: [1, 2, 3, 4, 5],
+      activeData,
 
       numberFormat
     }
@@ -155,16 +210,35 @@ export default defineComponent({
 <style scoped lang="less">
 @import "~@/assets/styles/variables";
 
+.active-chart-legend {
+  position: relative;
+  height: 20px;
+  margin-top: 8px;
+  font-size: 0;
+  line-height: 20px;
+
+  span {
+    display: inline-block;
+    width: 33.33%;
+    font-size: 12px;
+    text-align: center;
+
+    &:first-child {
+      text-align: left;
+    }
+
+    &:last-child {
+      text-align: right;
+    }
+  }
+}
+
 .map-chart {
+  position: relative;
+  width: 100%;
   height: 452px;
   padding-top: 24px;
   text-align: center;
-
-  img {
-    display: inline-block;
-    max-width: 100%;
-    max-height: 437px;
-  }
 }
 
 .pie-card ::v-deep(.pie-stat) {
